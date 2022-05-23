c5603c4b66dfb8fda6dbc9838121a57038637db9
Xodarap7 <basyar86@gmail.com>
2022-05-23 10:45:25 +0500

    make logger settings

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -1,1 +1,9 @@
+import logging
 from page_loader.download import download
+
+log_level = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+}
--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -1,8 +1,27 @@
-from page_loader import download
+import logging
+import os
+
+from page_loader import download, log_level
 from page_loader.cli import parse_args
 
 
 def main():
+    level = log_level.get(os.environ.get('PAGE_LOADER_LOG'))
+    file_name = os.environ.get('PAGE_LOADER_LOG_DESTINATION')
+    if level:
+        if file_name:
+            logging.basicConfig(
+                level=level,
+                format='%(asctime)s %(levelname)s %(module)s %(message)s',
+                filename='page_loader.log',
+                filemode='w',
+            )
+        else:
+            logging.basicConfig(
+                level=level,
+                format='%(asctime)s %(levelname)s %(module)s %(message)s',
+            )
+
     args = parse_args()
     print(download(args.page, args.output))
 
